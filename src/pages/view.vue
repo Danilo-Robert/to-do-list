428ee68067c4f8d05608d8930c68d6aa6032df37
danilo-robert <99danilorobert@gmail.com>
2026-03-31 10:32:02 -0400

    estilização do menu

--- src/pages/view.vue
+++ src/pages/view.vue
@@ -1,58 +1,38 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer">
-      <RouterLink to="/">Go to Home</RouterLink>
-      <RouterLink to="/about">Go to About</RouterLink>
-
-      <RouterLink v-for="(item, i) in items"
-          :key="i"
-          :to="item.to">
-        <v-list-item
-          
-          :value="item"
-          color="primary"
-        >
-          <template v-slot:prepend>
-            <v-icon :icon="item.icon"></v-icon>
-          </template>
-
-          <v-list-item-title v-text="item.text"></v-list-item-title>
-        </v-list-item>
-      </RouterLink>
+      
       <v-list>
         <v-list-item
-          prepend-avatar="https://cdn.vuetifyjs.com/images/john.png"
-          subtitle="john@google.com"
-          title="John Leider"
+          prepend-avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfdBt4R7qOfuZFMK7Wm80Xg_0Di3ZOcPjfuw&s"
+          subtitle="Quaterback 1"
+          title="Patrick Mahomes III"
         >
-          <template v-slot:append>
-            <v-btn icon="mdi-menu-down" size="small" variant="text"></v-btn>
-          </template>
-        </v-list-item>
+      </v-list-item>
       </v-list>
 
       <v-divider></v-divider>
 
       <v-list :lines="false" density="compact" nav>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :value="item"
-          color="primary"
-        >
-          <template v-slot:prepend>
-            <v-icon :icon="item.icon"></v-icon>
-          </template>
+        <RouterLink 
+        v-for="(item, i) in items" 
+        :key="i"
+        :to="item.to">
+          <v-list-item :value="item" color="primary">
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
 
-          <v-list-item-title v-text="item.text"></v-list-item-title>
-        </v-list-item>
+            <v-list-item-title :textContent="item.text"></v-list-item-title>
+          </v-list-item>
+        </RouterLink>
       </v-list>
     </v-navigation-drawer>
 
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-app-bar-title>Application</v-app-bar-title>
+      <v-app-bar-title>To do List</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
@@ -67,7 +47,14 @@ import { ref } from "vue";
 const drawer = ref(null);
 
 const items = [
-    { text: 'Home', icon: 'mdi-folder', to: '/' },
-    { text: 'About', icon: 'mdi-account-multiple', to: '/about' }
-  ];
+  { text: "Home", icon: "mdi-folder", to: "/" },
+  { text: "About", icon: "mdi-account-multiple", to: "/about" },
+];
 </script>
+
+<style scoped>
+  a{
+    text-decoration: none;
+    color: #000000
+  }
+</style>
